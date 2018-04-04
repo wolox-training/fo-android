172e516e2c105ccedd7e9547b83badf0151444fa
foviedoITBA <foviedo@itba.edu.ar>
2018-04-04 08:58:55 -0300

    Login screen layout (#1)
    * Login screen layout
    
    * Group libraries together
    
    * Add spacing and font size resources file
    
    * Turn all XML values into lowercase
    
    * Add space between last attribute and closing tag in XML files
    
    * Reorder XML attributes

---
+++ app/src/main/java/ar/com/wolox/android/foandroid/ui/LoginActivity.java
@@ -0,0 +1,15 @@
+package ar.com.wolox.android.foandroid.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import ar.com.wolox.android.foandroid.R;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
