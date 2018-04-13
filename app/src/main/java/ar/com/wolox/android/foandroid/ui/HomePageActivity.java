066a9418680a8292b9307e92ea57df2e102f5dd2
foviedoITBA <foviedo@itba.edu.ar>
2018-04-13 14:20:04 -0300

    Add mock HomeActivity with skeleton functionality

--- app/src/main/java/ar/com/wolox/android/foandroid/ui/HomePageActivity.java
+++ app/src/main/java/ar/com/wolox/android/foandroid/ui/HomePageActivity.java
@@ -1,8 +1,13 @@
 package ar.com.wolox.android.foandroid.ui;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentManager;
 import android.content.Context;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
 
 import ar.com.wolox.android.foandroid.R;
 import ar.com.wolox.wolmo.core.activity.WolmoActivity;
@@ -14,14 +19,18 @@ import butterknife.BindView;
 
 public class HomePageActivity extends WolmoActivity {
 
-    @BindView(R.id.activity_blank_welcome_message) TextView mWelcomeMessage;
+    @BindView(R.id.activity_home_toolbar)
+    protected Toolbar mToolbar;
+    @BindView(R.id.activity_home_tab_layout)
+    protected TabLayout mTabLayout;
+    @BindView(R.id.activity_home_view_pager)
+    protected ViewPager mViewPager;
 
     @Override
     protected void init() {
-        mWelcomeMessage.setText(
-                getSharedPreferences(SP_DEFAULT, Context.MODE_PRIVATE)
-                        .getString(SP_KEY_USER, null)
-        );
+        setSupportActionBar(mToolbar);
+        mViewPager.setAdapter(new HomePagerAdapter(getSupportFragmentManager()));
+        mTabLayout.setupWithViewPager(mViewPager);
     }
 
     @Override
@@ -31,7 +40,33 @@ public class HomePageActivity extends WolmoActivity {
 
     @Override
     protected int layout() {
-        return R.layout.activity_blank;
+        return R.layout.activity_home;
     }
 
+    private static class HomePagerAdapter extends FragmentPagerAdapter {
+
+        HomePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch(position) {
+                case NEWS_POSITION:
+                    return new NewsFragment();
+                case PROFILE_POSITION:
+                    return new NewsFragment();
+                default:
+                    throw new RuntimeException("Invalid position " + position + " for HomePagerAdapter#getItem");
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+    }
+
+    private static final int NEWS_POSITION = 0;
+    private static final int PROFILE_POSITION = 1;
 }
