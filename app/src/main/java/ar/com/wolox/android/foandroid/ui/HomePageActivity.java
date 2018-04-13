1642473416e22b109a660193677b70ac5bccb38d
foviedoITBA <foviedo@itba.edu.ar>
2018-04-13 16:12:28 -0300

    Add icons, logo, title and mock fragment for HomeActivity

--- app/src/main/java/ar/com/wolox/android/foandroid/ui/HomePageActivity.java
+++ app/src/main/java/ar/com/wolox/android/foandroid/ui/HomePageActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
 
 import ar.com.wolox.android.foandroid.R;
@@ -26,11 +27,16 @@ public class HomePageActivity extends WolmoActivity {
     @BindView(R.id.activity_home_view_pager)
     protected ViewPager mViewPager;
 
+    private final PageDescriptor[] mPageDescriptors = {
+            new PageDescriptor(NewsFragment.class, R.drawable.ic_news_list_on, R.drawable.ic_news_list_off),
+            new PageDescriptor(ProfileFragment.class, R.drawable.ic_profile_on, R.drawable.ic_profile_off)
+    };
+
     @Override
     protected void init() {
         setSupportActionBar(mToolbar);
-        mViewPager.setAdapter(new HomePagerAdapter(getSupportFragmentManager()));
-        mTabLayout.setupWithViewPager(mViewPager);
+        setUpActionBar();
+        setUpTabLayout();
     }
 
     @Override
@@ -43,7 +49,44 @@ public class HomePageActivity extends WolmoActivity {
         return R.layout.activity_home;
     }
 
-    private static class HomePagerAdapter extends FragmentPagerAdapter {
+    private void setUpActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setIcon(R.drawable.wolox_logo);
+        actionBar.setTitle(R.string.wolox);
+    }
+
+    private void setUpTabLayout() {
+        mViewPager.setAdapter(new HomePagerAdapter(getSupportFragmentManager()));
+        mTabLayout.setupWithViewPager(mViewPager);
+        mTabLayout.getTabAt(0).setIcon(mPageDescriptors[0].mSelectedIcon);
+        for (int i = 1; i < mPageDescriptors.length; i++) {
+            mTabLayout.getTabAt(i).setIcon(mPageDescriptors[i].mUnselectedIcon);
+        }
+
+        mTabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                int position = tab.getPosition();
+                mTabLayout.getTabAt(position).setIcon(mPageDescriptors[position].mSelectedIcon);
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                int position = tab.getPosition();
+                mTabLayout.getTabAt(position).setIcon(mPageDescriptors[position].mUnselectedIcon);
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+
+        });
+    }
+
+    private class HomePagerAdapter extends FragmentPagerAdapter {
 
         HomePagerAdapter(FragmentManager fm) {
             super(fm);
@@ -51,22 +94,34 @@ public class HomePageActivity extends WolmoActivity {
 
         @Override
         public Fragment getItem(int position) {
-            switch(position) {
-                case NEWS_POSITION:
-                    return new NewsFragment();
-                case PROFILE_POSITION:
-                    return new NewsFragment();
-                default:
-                    throw new RuntimeException("Invalid position " + position + " for HomePagerAdapter#getItem");
+
+            try {
+                return mPageDescriptors[position].mFragmentClass.newInstance();
+            } catch (Exception e) {
+                throw new RuntimeException("Reflective fragment instantiation failed", e);
             }
+
         }
 
         @Override
         public int getCount() {
-            return 2;
+            return mPageDescriptors.length;
+        }
+
+    }
+
+    private static class PageDescriptor {
+        public Class<? extends Fragment> mFragmentClass;
+        public int mSelectedIcon;
+        public int mUnselectedIcon;
+
+        PageDescriptor(Class<? extends Fragment> fragmentClass,
+                              int selectedIcon,
+                              int unselectedIcon) {
+            mFragmentClass = fragmentClass;
+            mSelectedIcon = selectedIcon;
+            mUnselectedIcon = unselectedIcon;
         }
     }
 
-    private static final int NEWS_POSITION = 0;
-    private static final int PROFILE_POSITION = 1;
 }
