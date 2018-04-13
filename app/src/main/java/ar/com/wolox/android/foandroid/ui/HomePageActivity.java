193d3e6427846bf12e7f12d06e7217745d10971a
foviedoITBA <foviedo@itba.edu.ar>
2018-04-13 17:12:51 -0300

    Add title to tabs in HomeActivity

--- app/src/main/java/ar/com/wolox/android/foandroid/ui/HomePageActivity.java
+++ app/src/main/java/ar/com/wolox/android/foandroid/ui/HomePageActivity.java
@@ -9,6 +9,10 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import ar.com.wolox.android.foandroid.R;
 import ar.com.wolox.wolmo.core.activity.WolmoActivity;
@@ -28,8 +32,14 @@ public class HomePageActivity extends WolmoActivity {
     protected ViewPager mViewPager;
 
     private final PageDescriptor[] mPageDescriptors = {
-            new PageDescriptor(NewsFragment.class, R.drawable.ic_news_list_on, R.drawable.ic_news_list_off),
-            new PageDescriptor(ProfileFragment.class, R.drawable.ic_profile_on, R.drawable.ic_profile_off)
+            new PageDescriptor(NewsFragment.class,
+                                R.drawable.ic_news_list_on,
+                                R.drawable.ic_news_list_off,
+                                R.string.news),
+            new PageDescriptor(ProfileFragment.class,
+                                R.drawable.ic_profile_on,
+                                R.drawable.ic_profile_off,
+                                R.string.profile)
     };
 
     @Override
@@ -57,32 +67,32 @@ public class HomePageActivity extends WolmoActivity {
     }
 
     private void setUpTabLayout() {
-        mViewPager.setAdapter(new HomePagerAdapter(getSupportFragmentManager()));
+        HomePagerAdapter pagerAdapter = new HomePagerAdapter(getSupportFragmentManager());
+        mViewPager.setAdapter(pagerAdapter);
         mTabLayout.setupWithViewPager(mViewPager);
-        mTabLayout.getTabAt(0).setIcon(mPageDescriptors[0].mSelectedIcon);
-        for (int i = 1; i < mPageDescriptors.length; i++) {
-            mTabLayout.getTabAt(i).setIcon(mPageDescriptors[i].mUnselectedIcon);
+        for (int i = 0; i < mPageDescriptors.length; i++) {
+            mTabLayout.getTabAt(i).setCustomView(pagerAdapter.getTabView(i));
         }
-
+        ((ImageView)mTabLayout.getTabAt(0).getCustomView().findViewById(R.id.tab_icon))
+                .setImageResource(mPageDescriptors[0].mSelectedIcon);
         mTabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
 
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
                 int position = tab.getPosition();
-                mTabLayout.getTabAt(position).setIcon(mPageDescriptors[position].mSelectedIcon);
+                ((ImageView) tab.getCustomView().findViewById(R.id.tab_icon))
+                        .setImageResource(mPageDescriptors[position].mSelectedIcon);
             }
 
             @Override
             public void onTabUnselected(TabLayout.Tab tab) {
                 int position = tab.getPosition();
-                mTabLayout.getTabAt(position).setIcon(mPageDescriptors[position].mUnselectedIcon);
+                ((ImageView) tab.getCustomView().findViewById(R.id.tab_icon))
+                        .setImageResource(mPageDescriptors[position].mUnselectedIcon);
             }
 
             @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-
+            public void onTabReselected(TabLayout.Tab tab) {}
         });
     }
 
@@ -108,19 +118,29 @@ public class HomePageActivity extends WolmoActivity {
             return mPageDescriptors.length;
         }
 
+        public View getTabView(int position) {
+            View v = LayoutInflater.from(HomePageActivity.this).inflate(R.layout.tab_icon_and_title, null);
+            ((TextView) v.findViewById(R.id.tab_title)).setText(mPageDescriptors[position].mTitle);
+            ((ImageView) v.findViewById(R.id.tab_icon)).setImageResource(mPageDescriptors[position].mUnselectedIcon);
+            return v;
+        }
+
     }
 
     private static class PageDescriptor {
         public Class<? extends Fragment> mFragmentClass;
         public int mSelectedIcon;
         public int mUnselectedIcon;
+        public int mTitle;
 
         PageDescriptor(Class<? extends Fragment> fragmentClass,
                               int selectedIcon,
-                              int unselectedIcon) {
+                              int unselectedIcon,
+                              int title) {
             mFragmentClass = fragmentClass;
             mSelectedIcon = selectedIcon;
             mUnselectedIcon = unselectedIcon;
+            mTitle = title;
         }
     }
 
